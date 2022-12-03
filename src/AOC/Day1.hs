7e45923d3fb1680377c5407a93d8785faef5d6a5
Tobias Pflug <tobias.pflug@gmail.com>
2022-12-04 00:22:56 +0100

    solution for day1

--- src/AOC/Day1.hs
+++ src/AOC/Day1.hs
@@ -1,4 +1,19 @@
-module AOC.Day1 where
+module AOC.Day1 (findMax, findMaxTop3) where
 
-foo :: String
-foo = "foo"
+import qualified Data.List as L
+import qualified Data.Text as T
+
+findMax :: T.Text -> Integer
+findMax t = uncurry max $ foldr go (0, 0) (T.lines t)
+  where
+    go x (max, curr)
+      | x == "" = if curr > max then (curr, 0) else (max, 0)
+      | otherwise = (max, curr + read (T.unpack x))
+
+findMaxTop3 :: T.Text -> Integer
+findMaxTop3 t = get $ foldr go ([], 0) (T.lines t)
+  where
+    go x (m, curr)
+      | x == "" = (curr : m, 0)
+      | otherwise = (m, curr + read (T.unpack x) :: Integer)
+    get (max, curr) = sum . take 3 . L.sortBy (flip compare) $ curr : max
--- test/AOC/Day1Spec.hs
+++ test/AOC/Day1Spec.hs
@@ -1,11 +1,41 @@
 module AOC.Day1Spec (spec) where
 
 import AOC.Day1
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Test.Hspec
+
+caloriesSample :: T.Text
+caloriesSample =
+  T.unlines
+    [ "1000",
+      "2000",
+      "3000",
+      "",
+      "4000",
+      "",
+      "5000",
+      "6000",
+      "",
+      "7000",
+      "8000",
+      "9000",
+      "",
+      "10000"
+    ]
 
 spec :: Spec
 spec = do
   describe "Day1" $ do
-    describe "dummy" $ do
-      it "foo shouldbe foo" $ do
-        foo `shouldBe` "foo"
+    describe "findMax" $ do
+      it "should find the maximum for test input" $ do
+        findMax caloriesSample `shouldBe` 24000
+      it "should find the maximum for my puzzle input" $ do
+        input <- TIO.readFile "./data/day1.txt"
+        findMax input `shouldBe` 70116
+    describe "findMaxTop3" $ do
+      it "should find the top3 maximum for the test input" $ do
+        findMaxTop3 caloriesSample `shouldBe` 45000
+      it "should find the top3 maximum for my puzzle input" $ do
+        input <- TIO.readFile "./data/day1.txt"
+        findMaxTop3 input `shouldBe` 206582
